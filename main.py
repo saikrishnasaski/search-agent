3088fe2198e11b0bc7e10eca03fb0b89252bb999
krishna <saikrishnachitti19@gmail.com>
2026-05-30 13:54:17 +0530

    added tool definitions and agent skeletion

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from search-agent!")
+from unittest import result
+
+from dotenv import load_dotenv
+from langchain import tools
+from langchain.tools import tool
+from langsmith import traceable
+load_dotenv()
+
+MAX_ITERATIONS = 10
+
+# ---- Tools ----- #
+
+@tool
+def get_product_price(product: str) -> float:
+    """ Get the price of a product from the following dictionary """
+    product_prices = { "laptop": 1299.99, "pixel phone": 999.99, "samsung tv": 1000.99}
+    return product_prices[product, 0]
+
+@tool
+def apply_discound_based_on_tier(price: float, tier: str) -> float:
+    """ Apply the discound based on the tier and return final price """
+    tiers = { "gold": 20, "silver": 10, "bronze": 5 }
+    discount = tiers[tier, 0]
+    final_price = round((price - (price * discount / 100)), 2)
+    return final_price
 
 
+# --- Agent Loop --- #
+
+@traceable(name="Langchain Agent Loop")
+def run_agent(question: str):
+    pass
+
+
+
+
+# --- Main function --- #
 if __name__ == "__main__":
-    main()
+    print("Hello from agent!")
+    result = run_agent("what is the price of a pixel phone after applying gold tier discount?")
+    print(result)
